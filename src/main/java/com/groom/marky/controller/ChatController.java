f7b6c60756da93327dfbc36a1aa929de789473b1
김덕구 <gimdeoggu@gimdeogguui-MacBookPro.local>
2025-06-04 15:43:00 +0900

    cafe search tool

--- src/main/java/com/groom/marky/controller/ChatController.java
+++ src/main/java/com/groom/marky/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.groom.marky.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ChatController {
 
 	private final ChatClient chatClient;
@@ -18,10 +20,19 @@ public class ChatController {
 
 	@GetMapping("/ai")
 	public String chat(@RequestParam String message) {
-		return chatClient.prompt()
-			.user(message)
-			.call()
-			.content();
+
+		long start = System.currentTimeMillis();
+
+		String response =  chatClient.prompt()
+				.user(message)
+				.call()
+				.content();
+
+		long end = System.currentTimeMillis();
+
+		log.info("요청 처리 시간: {} ms", end-start);
+
+		return response;
 	}
 
 }
